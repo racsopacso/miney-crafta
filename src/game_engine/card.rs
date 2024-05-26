f9f9adfa0e5b1a02761b53ad958769a4906248c5
bhack <benwjhack@gmail.com>
2024-05-26 17:25:04 +0100

    add gitignore

---
+++ src/game_engine/card.rs
@@ -0,0 +1,14 @@
+use rand::prelude::*;
+
+#[derive(Debug)]
+pub struct Card {
+    attack: u8,
+    defense: u8,
+}
+
+pub fn generate_card() -> Card {
+    let attack = random::<u8>() % 5;
+    let defense = random::<u8>() % 5;
+    let card = Card { attack, defense };
+    return card;
+}
